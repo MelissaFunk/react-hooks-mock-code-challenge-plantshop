79291f82efa2fe7bfcf27bda91df954566f47d5b
+ <+>
2021-09-09 08:26:16 -0700

    completed lab

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plantsToDisplay }) {
+
+  const PlantCards = plantsToDisplay.map(plant =>
+    <PlantCard
+      key={plant.id}
+      plant={plant}
+    />
+    )
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{PlantCards}</ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [filterBy, setFilterBy] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(res => res.json())
+    .then(setPlants)
+  }, [])
+
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant])
+  }
+
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value)
+  }
+
+  const plantsToDisplay = plants.filter(plant => {
+    if (filterBy === "") {
+      return true
+    } else {
+      return plant.name === filterBy
+    }
+  })
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search handleFilterChange={handleFilterChange}/>
+      <PlantList plantsToDisplay={plantsToDisplay}/>
     </main>
   );
 }
